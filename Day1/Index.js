c2477e5f6aaa8a56bdf83710c138d36440c231ac
charlesm-916 <charlesmutsu16@gmail.com>
2023-12-20 11:21:36 +0530

    Upto Day3

--- Day1/Index.js
+++ Day1/Index.js
@@ -15,4 +15,18 @@ function partOne(file) {
 
 }
 
-console.log(partOne('./Input.txt'))
+function partTwo(file) {
+    const lines = fs.readFileSync(file, 'utf-8').trim().split('\n')
+    const values = lines
+    .map((line) => {
+        let value = 0
+        let first = line.split('').find((v) => !Number.isNaN(Number(v)))
+    
+        let last = line.split('').findLast((v) => !Number.isNaN(Number(v)))
+
+        return Number(first + last)
+    })
+    return values.reduce((s, v) => s + v)
+
+}
+console.log(partOne('./Example.txt'))
